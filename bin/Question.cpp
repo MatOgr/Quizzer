afb191875a5e4ed2eb342894e04018b807265c74
MatOgr <ogrodowczykmat@gmail.com>
2021-02-15 15:01:00 +0100

    Question update

--- bin/Question.cpp
+++ bin/Question.cpp
@@ -1,10 +1,18 @@
 #include "Question.hpp"
 
 Question::Question() : 
-    content("What does the foxes say?"), topic("life"), points(100) {};
+    content("What does the foxes say?"), 
+    answers("Hau;Miau;Kszksz;Aproksymacja"),
+    correct_answer(3),
+    topic("life"), 
+    points(100) {};
 
-Question::Question(const string cont, const string topic, const int pts) :
-    content(cont), topic(topic), points(pts) {};
+Question::Question(const string cont, const string answs, const int ans_nr, const string topic, const int pts) :
+    content(cont), 
+    answers(answs), 
+    correct_answer(ans_nr), 
+    topic(topic), 
+    points(pts) {};
 
 string Question::getContent() {
     return this->content;
@@ -14,8 +22,20 @@ string Question::getTopic() {
     return this->topic;
 }
 
+string Question::getAnswers() {
+    return answers;
+}
+
+bool Question::verify(const int nr) {
+    return nr == correct_answer;
+}
+
 int Question::getPoints() {
     return this->points;
+}
+
+void Question::setAnswers(const string answs) {
+    answers = answs;
 }
 
 void Question::setContent(const string cont) {
