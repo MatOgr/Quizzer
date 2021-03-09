3cd1ab9da214fa56cae15bcf2331487f1d8d2af7
Patryk Kaszuba <31697346+PatrykKaszuba@users.noreply.github>
2021-03-09 23:49:24 +0100

    gui update

--- client/main.py
+++ client/main.py
@@ -18,6 +18,7 @@ class Thread(QThread):
     loadRoomSig = pyqtSignal()
     updateRoomSig = pyqtSignal(str)
     QuestionSig = pyqtSignal(str)
+    FinalRankingSig = pyqtSignal(str)
 
     def __init__(self):
         super(Thread, self).__init__()
@@ -36,6 +37,8 @@ class Thread(QThread):
                 self.updateRoomSig.emit(self.buffer[2:])
             elif self.buffer[:1]=='?':
                 self.QuestionSig.emit(self.buffer[1:])
+            elif self.buffer[:1]=='~':
+                self.FinalRankingSig.emit(self.buffer[1:])
             else:
                 self.msgSig.emit(self.buffer)
 
@@ -56,6 +59,7 @@ class MainWindow(QMainWindow):
         self.thread.loadRoomSig.connect(self.switchRoom)
         self.thread.updateRoomSig.connect(self.updateRoom)
         self.thread.QuestionSig.connect(self.gameQuestion)
+        self.thread.FinalRankingSig.connect(self.showFinalRanking)
         self.counter=QTimer()
         self.counter.timeout.connect(self.countdown)
 
@@ -91,6 +95,10 @@ class MainWindow(QMainWindow):
         self.PushButton1.move(290, 380)
         self.PushButton1.clicked.connect(self.loginClicked)
 
+        self.IpLabel = QLabel(self.stack1)
+        self.IpLabel.setText("IP:")
+        self.IpLabel.move(240, 153)
+
         self.IpBox = QLineEdit(self.stack1)
         self.IpBox.move(260, 150)
         self.IpBox.setText("127.0.0.1")
@@ -99,15 +107,27 @@ class MainWindow(QMainWindow):
         self.PortBox.move(260, 200)
         self.PortBox.setText("8081")
 
+        self.PortLabel = QLabel(self.stack1)
+        self.PortLabel.setText("Port:")
+        self.PortLabel.move(232, 203)
+
         self.NicknameBox = QLineEdit(self.stack1)
         self.NicknameBox.move(260, 250)
         self.NicknameBox.setText("Name")
+
+        self.NicknameLabel = QLabel(self.stack1)
+        self.NicknameLabel.setText("Nickname:")
+        self.NicknameLabel.move(208, 253)
     def lobbyUi(self):
         self.stack2.resize(640, 480)
 
         self.ListWidget = QListWidget(self.stack2)
         self.ListWidget.resize(300, 200)
         self.ListWidget.move(180, 100)
+
+        self.RoomsListLabel = QLabel(self.stack2)
+        self.RoomsListLabel.setText("Rooms list:")
+        self.RoomsListLabel.move(180, 80)
 
         self.JoinButton = QtWidgets.QPushButton(self.stack2)
         self.JoinButton.setText("Join room")
@@ -138,18 +158,22 @@ class MainWindow(QMainWindow):
         self.AnswerAButton = QtWidgets.QPushButton(self.stack3)
         self.AnswerAButton.move(50, 250)
         self.AnswerAButton.resize(350,30)
+        self.AnswerAButton.clicked.connect(lambda: self.answerQuestion("1"))
 
         self.AnswerBButton = QtWidgets.QPushButton(self.stack3)
         self.AnswerBButton.move(50, 300)
         self.AnswerBButton.resize(350,30)
+        self.AnswerBButton.clicked.connect(lambda: self.answerQuestion("2"))
 
         self.AnswerCButton = QtWidgets.QPushButton(self.stack3)
         self.AnswerCButton.move(50, 350)
         self.AnswerCButton.resize(350,30)
+        self.AnswerCButton.clicked.connect(lambda: self.answerQuestion("3"))
 
         self.AnswerDButton = QtWidgets.QPushButton(self.stack3)
         self.AnswerDButton.move(50, 400)
         self.AnswerDButton.resize(350,30)
+        self.AnswerDButton.clicked.connect(lambda: self.answerQuestion("4"))
 
         self.ReadyButton = QtWidgets.QPushButton(self.stack3)
         self.ReadyButton.setText("Ready")
@@ -251,6 +275,9 @@ class MainWindow(QMainWindow):
 
     def questionAmount(self, limit):
         self.writeData(f"?{limit}:")
+
+    def addPoints(self, points):
+        self.writeData(f"%{points}:")
     
     def getRoomList(self):
         while(self.currentMenu == 1):
@@ -270,6 +297,7 @@ class MainWindow(QMainWindow):
 
     def switchRoom(self):
         self.ReadyButton.setEnabled(True)
+        self.gameStatus = False
         self.currentMenu = 2
         self.QtStack.setCurrentIndex(self.currentMenu)
         self.roomUpdateThread = threading.Thread(target=self.getRoomUpdate)
@@ -282,6 +310,7 @@ class MainWindow(QMainWindow):
         self.ReadyButton.setEnabled(False)
     
     def exitRoom(self):
+        self.gameStatus = False
         self.leaveRoom()
     
     def updateLobby(self, rooms):
@@ -296,16 +325,20 @@ class MainWindow(QMainWindow):
             self.ListWidget.setCurrentRow(selectedRoom)
 
     def updateRoom(self, data):
-        self.playerListWidget.clear()
-        data = data.split(':')
-        for player in data[3:]:
-            self.playerListWidget.addItem(player)
+        print("udate")
+        if self.gameStatus == False:
+            self.playerListWidget.clear()
+            data = data.split(':')
+            for player in data[3:]:
+                self.playerListWidget.addItem(player)
 
     def setPlayersLimit(self):
         self.playerLimit(self.PlayersLimitBox.text())
+        self.PlayersLimitBox.clear()
 
     def setQuestionsLimit(self):
         self.questionAmount(self.QuestionsLimitBox.text())
+        self.QuestionsLimitBox.clear()
     
     def setCategory(self):
         self.changeCategory(self.QuestionListBox.currentText())
@@ -331,13 +364,23 @@ class MainWindow(QMainWindow):
         self.AnswerBButton.setVisible(s)
         self.AnswerCButton.setVisible(s)
         self.AnswerDButton.setVisible(s)
+
+    def answerButtons(self, s:bool):
+        self.AnswerAButton.setEnabled(s)
+        self.AnswerAButton.setEnabled(s)
+        self.AnswerAButton.setEnabled(s)
+        self.AnswerBButton.setEnabled(s)
+        self.AnswerCButton.setEnabled(s)
+        self.AnswerDButton.setEnabled(s)
     
     def gameQuestion(self, buffer):
-        self.questionTs = time.time()
-        buffer = buffer.split(':')
+        self.gameStatus = True
+        buffer = buffer.split('#')
+        ranking = buffer[1]
+        buffer = buffer[0].split(':')
         question = buffer[0]
         answers = buffer[1].split('?')
-        self.correctAnswer = buffer[2]
+        self.correctAnswer = buffer[2][0]
         self.QuestionText.clear()
         self.QuestionText.insertPlainText(question)
         self.AnswerAButton.setText(answers[0])
@@ -345,8 +388,10 @@ class MainWindow(QMainWindow):
         self.AnswerCButton.setText(answers[2])
         self.AnswerDButton.setText(answers[3])
         self.showSettingsUi(False)
+        self.answerButtons(True)
         self.showGameUi(True)
-        self.timeLeft = 5
+        self.updatePlayerRanking(ranking)
+        self.timeLeft = 10
         self.TimeCounterLabel.setText(f"00:10")
         self.counter.start(1000)
     
@@ -356,7 +401,30 @@ class MainWindow(QMainWindow):
             self.TimeCounterLabel.setText(f"00:0{self.timeLeft}")
         else:
             self.counter.stop()
+            self.answerButtons(False)
     
+    def answerQuestion(self, answer):
+            self.answerButtons(False)
+            print(answer)
+            print(self.correctAnswer)
+            print(answer == self.correctAnswer)
+            if answer == self.correctAnswer:
+                self.addPoints(self.timeLeft)
+                print(f"Odp {self.timeLeft}")
+                
+    def updatePlayerRanking(self, ranking):
+        ranking = ranking.replace('\t','').replace(':','').split('\n')
+        ranking = list(filter(None, ranking))
+        self.playerListWidget.clear()
+        for player in ranking:
+            self.playerListWidget.addItem(player)
+        print(ranking)
+
+    def showFinalRanking(self, ranking):
+        self.gameStatus = False
+        self.ReadyButton.setEnabled(True)
+        self.msgBox(ranking)
+
 def main():
     app = QApplication(sys.argv)
     showMain = MainWindow()
